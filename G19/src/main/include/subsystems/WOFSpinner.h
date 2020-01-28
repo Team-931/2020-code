ccb40566f1e4b5d03ff7a78d1f37d6da0e55064d
P. I. Harrington <71712.2415@compuserve.com>
2020-01-27 19:33:11 -0600

    Redid directory structure

--- G19/src/main/include/subsystems/WOFSpinner.h
+++ G19/src/main/include/subsystems/WOFSpinner.h
@@ -10,6 +10,7 @@
 #include <frc/encoder.h>
 #include <rev/ColorSensorV3.h>
 #include <frc2/command/SubsystemBase.h>
+#include <frc/Controller/PIDController.h>
 #include <CTRE/phoenix.h>
 #include "Constants.h"
 
@@ -29,6 +30,8 @@ class WOFSpinner : public frc2::SubsystemBase {
 
   frc::Encoder Encoder{Encoder1, Encoder2};
   rev::ColorSensorV3 ColorSensor{frc::I2C::Port::kOnboard};
+  frc2::PIDController pid{1,0,0};
+  units::radians theta;
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
 };
